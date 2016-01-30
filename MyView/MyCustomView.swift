9ad429cdff45a6006ea732565334ad742c65fb9a
henanzhg <854702773@qq.com>
2016-01-30 15:08:25 +0800

    first commit

---
+++ MyView/MyCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  MyCustomView.swift
+//  testCustomView
+//
+//  Created by mac on 1/30/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
